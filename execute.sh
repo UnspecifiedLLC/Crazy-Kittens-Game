a64c3c452c5e083cc4072f38919d0c60f2f84a3b
Dawn Test (Cannan) Code <dawn.test.code@gmail.com>
2017-09-30 22:19:33 -0400

    Updating for cross platform

--- execute.sh
+++ execute.sh
@@ -1,4 +1,4 @@
-sudo npm install live-server -g
+sudo npm install http-server -g
 google-chrome http://127.0.0.1:8080 &>/dev/null
-open http://127.0.0.1:8080 &>/dev/null
-live-server .
+open http://127.0.0.1:8080& &>/dev/null
+http-server .
